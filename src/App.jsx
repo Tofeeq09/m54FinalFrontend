4840a993209757d5cb6e92a294d6d3677b5df4b7
Your Name <you@example.com>
2024-03-06 16:43:53 +0000

    FEC1: Navbar Component

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import Dashboard from "./pages/dashboard/Dashboard";
 import SignupPage from "./pages/signup/SignupPage";
 import LoginPage from "./pages/login/LoginPage";
 import LandingPage from "./pages/landing/LandingPage";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/home" element={<Dashboard />} />
