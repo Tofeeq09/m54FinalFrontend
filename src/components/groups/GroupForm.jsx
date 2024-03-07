a56048f118738a4958c44c881e09bbbc7ecbedbd
liamcunniffe <liamcunniffe88@outlook.com>
2024-03-07 23:35:20 +0000

    created modal and group form

--- src/components/groups/GroupForm.jsx
+++ src/components/groups/GroupForm.jsx
@@ -1,10 +1,48 @@
+import React from "react";
 import { useState } from "react";
-import ReactDom from "react-dom";
+import { useNavigate } from "react-router-dom";
+import Modal from "react-modal";
 
-function GroupForm() {
-  const [inputs, setInputs] = useState({});
-
-  const handleChange = (event) => {
-    const name = event.target.name;
-  };
+function openModal() {
+  setIsOpen(true);
 }
+
+function closeModal() {
+  setIsOpen(false);
+}
+
+const GroupForm = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(inputs);
+  };
+  return (
+    <div>
+      <button onClick={openModal}>Open Group Form</button>
+      <Modal isOpen={openModal} onRequestClose={closeModal}>
+        <button onClick={closeModal}>Close</button>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Enter your username:
+            <input
+              type="text"
+              name="username"
+              value={inputs.username || ""}
+              onChange={handleChange}
+            />
+          </label>
+          <label>
+            Enter a Group:
+            <input
+              type="text"
+              name="groupname"
+              value={inputs.groupname || ""}
+              onChange={handleChange}
+            />
+          </label>
+          <input type="submit" />
+        </form>
+      </Modal>
+    </div>
+  );
+};
