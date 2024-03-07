42a25ce50a0bfcec05e792e18924838b5fa20d94
liamcunniffe <liamcunniffe88@outlook.com>
2024-03-07 11:59:31 +0000

    created groups folder for group form

---
+++ src/components/groups/GroupForm.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import ReactDom from "react-dom";
+
+function GroupForm() {
+  const [inputs, setInputs] = useState({});
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+  };
+}
