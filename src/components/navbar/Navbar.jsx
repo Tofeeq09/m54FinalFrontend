a1e3e7faf02d937cb12aba1119fe7654833ab3db
SAM101013 <amoonmarketing@proton.me>
2024-03-08 14:15:13 +0000

    navrbarstyling

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,15 @@
 import { Link } from "react-router-dom";
 import React from "react";
+import logo from "./logo.jpeg"; // Adjust the path to match the location of logo.jpeg
+
 import "./Navbar.scss";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
+      <div className="logo-container">
+        <img src={logo} alt="Logo" className="logo" />
+      </div>
       <Link to="/" className="homepage">
         Homepage
       </Link>
