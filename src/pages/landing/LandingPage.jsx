cacff85b47386fa77c9569e5b0aeec641b6acbbb
SAM101013 <amoonmarketing@proton.me>
2024-03-08 11:59:51 +0000

    eventsformupdate

--- src/pages/landing/LandingPage.jsx
+++ src/pages/landing/LandingPage.jsx
@@ -1,5 +1,34 @@
+import React, { useState } from "react";
+import EventsForm from "../events/eventsform";
+
 const LandingPage = () => {
-  return <div>LandingPage</div>;
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <div className="landing-page">
+      <h1>Welcome Squads</h1>
+      <p>The place where tech connects people</p>
+      <button onClick={openModal}>Open Events Modal</button>
+      {isModalOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={closeModal}>
+              &times;
+            </span>
+            <EventsForm closeModal={closeModal} />
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default LandingPage;
